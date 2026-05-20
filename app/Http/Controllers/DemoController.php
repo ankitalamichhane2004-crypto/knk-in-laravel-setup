c250b2d67188fe8435ffa94fb286186f74b47953
ankitalamichhane2004-crypto <ankitalamichhane2004@gmail.com>
2026-05-20 21:22:57 +0545

    certain changes

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -12,9 +12,7 @@ class DemoController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+   
     public function create()
     {
         //
