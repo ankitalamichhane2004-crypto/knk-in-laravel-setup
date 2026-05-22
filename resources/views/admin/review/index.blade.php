65bc36a90714747d37d42831c2dd0181b296f0f6
ankitalamichhane2004-crypto <ankitalamichhane2004@gmail.com>
2026-05-22 11:44:56 +0545

    complete review crud

--- resources/views/admin/review/index.blade.php
+++ resources/views/admin/review/index.blade.php
@@ -43,12 +43,15 @@
         
       <tr>
         <td>{{ $data->CustomerName }}</td>
-        <td>{{ $data->Rating}}</td>
+        <td>{{ $data->Rating }} </td>
         <td>{{ $data->Description }} </td>
-        <td><a href="{{ route('blogshow', $data->id) }}"><button>Show</button></a></td>
-        <td><a href="{{ route('blogedit', $data->id) }}"><button>Edit</button></td></a></td>
-        <td><form action="{{ route('blogdelete',$data->id) }}" method="POST">@csrf<button type="submit">Delete</button></form></td>
-     <td><a href="{{ route('blogform', $data->id) }}"><button>Create</button></a></td>
+        <td><a href="{{ route('review.show', $data->id) }}"><button>Show</button></a></td>
+        <td><a href="{{ route('review.edit', $data->id) }}"><button>Edit</button></td></a></td>
+        <td><form action="{{ route('review.destroy',$data->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Delete</button></form></td>
+     <td><a href="{{ route('review.create') }}"><button>Create</button></a></td>
       </tr>
        
         @endforeach
